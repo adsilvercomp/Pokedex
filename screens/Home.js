788287fd8269a58d509fc6a090de6dc1d380042b
aaron silverstein <aaronsilverstein@aarons-MacBook-Pro.local>
2024-04-04 16:47:08 -0400

    successfully rendering list

--- screens/Home.js
+++ screens/Home.js
@@ -13,18 +13,19 @@ export default function Home(){
     },[])
 
     return(
-      <View style={styles.container}>
+      <View style={styles.listContainer}>
         {loading && <Text>Loading...</Text>}
 
         {error && <Text>Error Loading Data</Text>}
 
         {pokemonData !== undefined && (
           <FlatList 
+          style={styles.list}
           keyExtractor={(item => item.id)}
           data={pokemonData}
           renderItem={({item}) => (
-              <TouchableOpacity>
-                <Image source={{ uri: item.sprite }}/>
+              <TouchableOpacity style={styles.listItem}>
+                <Image style={styles.image} source={{ uri: item.sprite }}/>
                 <Text>{item.name}</Text>
               </TouchableOpacity>
           )}
@@ -36,11 +37,36 @@ export default function Home(){
 }
 
 const styles = StyleSheet.create({
-  container: {
+  listContainer: {
     flex:1,
     backgroundColor:'orange',
     justifyContent:'center',
     alignItems:'center',
-    paddingTop: 10
+
+  },
+
+  list: {
+    flex:1,
+    width: '100%',
+    height: '100%',
+    backgroundColor:'green',
+
+  },
+
+  listItem: {
+    padding:10,
+    height:100,
+    display:'flex',
+    flexDirection:'row',
+    justifyContent:'flex-start',
+    backgroundColor: 'gray',
+    borderBottomColor: 'black',
+    borderBottomWidth: 2
+  },
+
+  image:{
+    height: 'auto',
+    width: 70,
+   
   }
 })
